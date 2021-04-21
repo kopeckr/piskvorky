8429854a311ef959f7e1abfb778e0d27121ad3fc
Kristýna Kopečková <kopeckova92@gmail.com>
2021-04-21 15:32:34 +0200

    ulozeno js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ console.log('funguju');
 
 let naTahu = 'kolecko';
 
-// const btnElm = document.querySelectorAll('#pole');
-
 const tah = (event) => {
   if (naTahu === 'kolecko') {
     event.target.classList.add('board__field––circle');
@@ -19,8 +17,3 @@ let btnElm = document.querySelectorAll('#pole');
 for (let i = 0; i < btnElm.length; i += 1) {
   btnElm[i].addEventListener('click', tah);
 }
-
-// const btnElm2 = document.querySelectorAll('#pole');
-// for (let i = 0; i < btnElm2.length; i += 2) {
-//   btnElm2[i].addEventListener('click', vyberKrizek);
-// }
