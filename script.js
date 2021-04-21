2cf10d7afb419540564b614be3fe74ed6bc2f7e0
Kristýna Kopečková <kopeckova92@gmail.com>
2021-04-21 15:24:21 +0200

    Funguje stridani kolecka a krizku v hracim poli

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ console.log('funguju');
 
 let naTahu = 'kolecko';
 
-const vyberPole = (event) => {
-  event.target.classList.add('board__field--circle');
+// const btnElm = document.querySelectorAll('#pole');
+
+const tah = (event) => {
+  if (naTahu === 'kolecko') {
+    event.target.classList.add('board__field––circle');
+    naTahu = 'krizek';
+  } else {
+    event.target.classList.add('board__field––cross');
+    naTahu = 'kolecko';
+  }
 };
 
-const btnElm = document.querySelectorAll('#pole');
+let btnElm = document.querySelectorAll('#pole');
 for (let i = 0; i < btnElm.length; i += 1) {
-  btnElm[i].addEventListener('click', vyberPole);
+  btnElm[i].addEventListener('click', tah);
 }
+
+// const btnElm2 = document.querySelectorAll('#pole');
+// for (let i = 0; i < btnElm2.length; i += 2) {
+//   btnElm2[i].addEventListener('click', vyberKrizek);
+// }
