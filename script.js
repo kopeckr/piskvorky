ead8cca5b6865e484c05481bb401c7b0a22d095f
Kristýna Kopečková <kopeckova92@gmail.com>
2021-04-22 20:54:15 +0200

    finálni verze

--- script.js
+++ script.js
@@ -7,17 +7,19 @@ let hraje = document.querySelector('.kolecko');
 
 const tah = (event) => {
   if (naTahu === 'kolecko') {
-    event.target.classList.add('board__field––circle');
+    event.target.classList.add('policko__kolecko');
     naTahu = 'krizek';
     hraje.src = 'img/cross.svg';
   } else {
-    event.target.classList.add('board__field––cross');
+    event.target.classList.add('policko__krizek');
     naTahu = 'kolecko';
     hraje.src = 'img/circle.svg';
   }
+  event.target.removeEventListener('click', tah);
+  event.target.disabled = true;
 };
 
-let btnElm = document.querySelectorAll('#pole');
+let btnElm = document.querySelectorAll('#policko-id');
 for (let i = 0; i < btnElm.length; i += 1) {
   btnElm[i].addEventListener('click', tah);
 }
