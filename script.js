2e4b8f0397d505ab39b85c85fb8e078763f27b7a
Kristýna Kopečková <kopeckova92@gmail.com>
2021-04-22 18:58:14 +0200

    stridani O a X ve HRAJE

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ console.log('funguju');
 
 let naTahu = 'kolecko';
 
+let hraje = document.querySelector('.kolecko');
+
 const tah = (event) => {
   if (naTahu === 'kolecko') {
     event.target.classList.add('board__field––circle');
     naTahu = 'krizek';
+    hraje.src = 'img/cross.svg';
   } else {
     event.target.classList.add('board__field––cross');
     naTahu = 'kolecko';
+    hraje.src = 'img/circle.svg';
   }
 };
 
